fd9234832ee40f87c31f720b42ba4e9af432b646
yfwz100 <yfwz100@yeah.net>
2013-01-14 00:02:29 +0800

    Delete routes/user.js sample file.

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@
  * GET home page.
  */
 
-var user = require('./user')
-  , home = require('./home')
+var home = require('./home')
   , article = require('./article');
 
 exports.init = function init(app) {
   app.all('/', home.index);
-  app.get('/users', user.list);
   app.get('/no-interest', home.noInterest);
 
   app.all('/about', home.about);
