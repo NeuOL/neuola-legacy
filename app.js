65f5fe37abab06b2299a152e5726ed6f7cea899f
Zhi <yfwz100@yeah.net>
2013-01-25 13:03:07 +0800

    [Test] Testing windows git client.

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.map = function(a, route) {
     }
   }
 };
-//routes.init(app, '/');
+
 app.map({
   '/': routes
 });
@@ -65,3 +65,4 @@ app.map({
 http.createServer(app).listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
 });
+
